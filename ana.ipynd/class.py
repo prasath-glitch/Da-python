79f10bdacf7b85fe72b48bfbad4e787c4611f13d
prasath <prasathzeee@gmail.com>
2026-06-15 16:46:50 +0530

    propare.py

---
+++ ana.ipynd/class.py
@@ -0,0 +1,31 @@
+"""
+
+my_name= input("start")
+my_age=int(input("typr age bro :"))
+my_degree=input("with type degree :")
+my_num = int(input("type num here"))
+
+
+print("my_name",my_name)
+print("myage",my_age)
+print("my_degree",my_degree)
+print("my_num",my_num)
+print("type",type(my_name),type(my_age),type(my_degree),type(my_num))
+
+hari="boom "
+print=("lenght",len(hari))
+"""
+"""
+ask_name=input("hello")
+ask_city=input("welcome to :")
+
+print("hello",ask_name)
+print("welcome",ask_city)
+"""
+name=input("enter the name")
+city=input("enter the name")
+
+print("name",name)
+print("city",city)
+print(f"hello{name}")
+print(f"welcome to{city}")
---
+++ task 2.py
@@ -0,0 +1,93 @@
+                                # Task 1 : Movie Ticket Eligibility 
+
+
+name=input("enter name : ")
+age=(int(input("enter age :")))
+print(f"hello  {name}")
+if( age < 13):
+    print("  ticket Type: child ticket  ")
+elif(age >= 13 and age < 50 ):
+  print ("ticket Type : adult ticket")  
+
+elif(age >= 60):
+  print("ticket Type :senior citizen ticket")
+
+ 
+                         # Task 2: Temperature Checker
+
+temp=int(input("enter the num :"))
+
+if( temp < 20):
+       print(" Weather: very cold bro ")
+elif ( 20<= temp <=30 ):
+       print(" Weather: pleasant "
+             
+             )
+elif(temp > 30 ):
+       print(" Weather: hot ")
+        
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
